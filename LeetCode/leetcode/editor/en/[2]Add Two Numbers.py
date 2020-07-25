2ffa8938f561d893465c0766081d929c653e39fc
Wentao <biwentaofrank@gmail.com>
2020-07-25 14:02:01 -0400

    Test LeetCode Pycharm interface

---
+++ LeetCode/leetcode/editor/en/[2]Add Two Numbers.py
@@ -0,0 +1,28 @@
+# You are given two non-empty linked lists representing two non-negative integer
+# s. The digits are stored in reverse order and each of their nodes contain a sing
+# le digit. Add the two numbers and return it as a linked list. 
+# 
+#  You may assume the two numbers do not contain any leading zero, except the nu
+# mber 0 itself. 
+# 
+#  Example: 
+# 
+#  
+# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+# Explanation: 342 + 465 = 807.
+#  
+#  Related Topics Linked List Math 
+#  👍 8556 👎 2167
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+# leetcode submit region end(Prohibit modification and deletion)
