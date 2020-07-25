122ceb540892bd6dd7cc8f566d471b21427105dc
Wentao <biwentaofrank@gmail.com>
2020-07-25 14:13:05 -0400

    Test LeetCode Pycharm interface

---
+++ LeetCode/leetcode/editor/en/[1]Two Sum.py
@@ -0,0 +1,23 @@
+# Given an array of integers, return indices of the two numbers such that they a
+# dd up to a specific target. 
+# 
+#  You may assume that each input would have exactly one solution, and you may n
+# ot use the same element twice. 
+# 
+#  Example: 
+# 
+#  
+# Given nums = [2, 7, 11, 15], target = 9,
+# 
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+#  
+#  Related Topics Array Hash Table 
+#  👍 15844 👎 577
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+# leetcode submit region end(Prohibit modification and deletion)
