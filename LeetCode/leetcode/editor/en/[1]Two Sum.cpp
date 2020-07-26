3865325e71100d9780a5ca6114ee3c67842da15b
Wentao <biwentaofrank@gmail.com>
2020-07-25 23:45:04 -0400

    Add CLion configure settings

---
+++ LeetCode/leetcode/editor/en/[1]Two Sum.cpp
@@ -0,0 +1,26 @@
+//Given an array of integers, return indices of the two numbers such that they a
+//dd up to a specific target. 
+//
+// You may assume that each input would have exactly one solution, and you may n
+//ot use the same element twice. 
+//
+// Example: 
+//
+// 
+//Given nums = [2, 7, 11, 15], target = 9,
+//
+//Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
+// 
+// Related Topics Array Hash Table 
+// 👍 15848 👎 577
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
